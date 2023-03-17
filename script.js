555b78dd35f46c2e92864df7ebdbcda3267c1068
Artur250 <gameartur012@gmail.com>
2023-03-17 21:38:34 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,88 @@
+
+
+// const body = document.body 
+
+
+// const int1 = document.createElement('input') 
+// const p1 = document.createElement('p') 
+// p1.innerText = 'Введите ваше имя'
+
+// const int2 = document.createElement('input') 
+// const p2 = document.createElement('p') 
+// p2.innerText = 'Введите вашу фамилию'
+
+// const btn1 = document.createElement('button')
+// btn1.innerText = 'Отправить'
+
+
+// body.before(p1)
+// body.before(int1) 
+
+// body.prepend(int2)
+// body.prepend(p2)
+
+// body.after(btn1)
+ 
+  
+ 
+// const body = document.body 
+// const body1 = document.body1
+
+// const int1 = document.createElement('input') 
+// const p1 = document.createElement('p') 
+// p1.innerText = 'Введите ваше имя и фамилию;'
+
+// const int2 = document.createElement('input') 
+// const p2 = document.createElement('p') 
+// p2.innerText = 'Введите номер заказа:'
+
+
+// const int3 = document.createElement('input') 
+// const p3 = document.createElement('p') 
+// p3.innerText = 'Телефон:'
+
+// const int4 = document.createElement('input') 
+// const p4 = document.createElement('p') 
+// p4.innerText = 'Электронный адрес:'
+
+// const int5 = document.createElement('input') 
+// const p5 = document.createElement('p') 
+// p5.innerText = 'Введите сумму оплаты:'
+
+// const btn1 = document.createElement('button')
+// btn1.innerText = 'Перейти к оплате'
+// btn1.style.backgroundColor = 'red'
+
+// btn1.style.border = 'none'
+// btn1.style.padding = '18px 38px'
+
+
+
+
+// body.prepend(int4)
+// body.prepend(p4)
+
+// body.before(p1)
+// body.before(int1) 
+
+// body.prepend(int5)
+// body.prepend(p5)
+
+// body.append(p3)
+// body.append(int3) 
+
+// body.prepend(int2)
+// body.prepend(p2)
+
+// body.after(btn1)
+
+
+
+ //...перед объектом
+//textElement.before(newElement);
+//...после объекта
+//textElement.after(newElement);
+//...внутрь и в начало объекта
+//textElement.prepend(newElement);
+//...внутрь и в конец объекта
+//textElement.append(newElement);
